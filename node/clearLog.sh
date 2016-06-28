fcee4c7862492ef55cb3861369a21bfcddfc1251
wangkuan <wangkuan@souche.com>
2016-06-28 17:37:46 +0800

    add

--- node/clearLog.sh
+++
@@ -1,5 +0,0 @@
-cd ../
-rm -rf log
-mkdir log
-cd log/
-mkdir debug error info warn
--- node/common.js
+++
@@ -1,9 +0,0 @@
-global.Common = {
-	getRandom: function(Min, Max) {
-		Min = Min || 0;
-		Max = Max || 1;
-		var Range = Max - Min;
-		var Rand = Math.random();
-		return (Min + Math.round(Rand * Range));
-	}
-};
